ac6423eaf3fe3b14c2bd039dcba42d67a36d6536
GMatheusst <matheusaugustomendesramalho@gmail.com>
2024-10-31 15:16:03 -0300

    :pencil: docs (data.ts): remove documentação desnecessaria

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,15 +11,13 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
 
     console.log('Fetching revenue data...');
     await new Promise((resolve) => setTimeout(resolve, 30));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-     console.log('Data fetch completed after 3 seconds.');
+     console.log('Data fetch completed after 30 ms.');
 
     return data.rows;
   } catch (error) {
@@ -30,7 +28,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    // Fetch the last 5 invoices, sorted by date
     const data = await sql<LatestInvoiceRaw>`
     SELECT invoices.amount, customers.name, customers.image_url, customers.email
     FROM invoices
@@ -51,9 +48,6 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
